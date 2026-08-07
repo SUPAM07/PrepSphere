5bb6c2c54e2836ae6f424e1308a9b427ce08e870
SUPAM_07 <supamroy8@gmail.com>
2026-08-07 23:49:24 +0530

    Fix region mismatch by enforcing eu-north-1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   type        = string
   description = "AWS Region to deploy resource into"
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "project_name" {
