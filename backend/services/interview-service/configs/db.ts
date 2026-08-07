8eeecc0b18d6613ad9c3777a43fb8d7319ec7c78
SUPAM_07 <supamroy8@gmail.com>
2026-08-07 20:12:33 +0530

    feat(interview-service): add db.ts

---
+++ backend/services/interview-service/configs/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDb = async (): Promise<void> => {
+  try {
+    const mongoUrl = process.env.MONGODB_URL;
+    if (!mongoUrl) {
+      throw new Error("MONGODB_URL is not defined");
+    }
+    await mongoose.connect(mongoUrl);
+    console.log("Connected to MongoDB (Interview Service)");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+};
