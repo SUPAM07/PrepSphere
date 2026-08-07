d21e591dacf2253c5e400b822cae66d6461a5463
SUPAM_07 <supamroy8@gmail.com>
2026-08-07 14:29:50 +0530

    refactor(frontend): convert api/interview.api.js to api/interview.api.ts

--- frontend/src/api/interview.api.ts
+++ frontend/src/api/interview.api.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import api from "../utils/axios";
 
 
@@ -7,81 +6,42 @@ import api from "../utils/axios";
 // Start Interview
 // ---------------------------------------
 
-export const startInterview = async (data) => {
-
+export const startInterview = async (data: any) => {
   try {
     const response = await api.post(`/api/interview/start`,data);
-
   return response.data;
-  } catch (error) {
+  } catch (error: any) {
     console.log(error)
-
     return null
   }
-
-  
-
 };
 
-
-// ---------------------------------------
-// Submit Answer
-// ---------------------------------------
-
-export const submitAnswer = async (data) => {
-
+export const submitAnswer = async (data: any) => {
   try {
     const response = await api.post(`/api/interview/answer`,data);
-
   return response.data;
-  } catch (error) {
+  } catch (error: any) {
     console.log(error)
     return null
   }
-
-  
-
 };
 
-
-// ---------------------------------------
-// Get Single Interview
-// ---------------------------------------
-
-export const getInterview = async (id) => {
-
-
+export const getInterview = async (id: string) => {
   try {
     const response = await api.get(`/api/interview/${id}`);
-
   return response.data;
-    
-  } catch (error) {
+  } catch (error: any) {
     console.log(error)
     return null
   }
-
-  
-
 };
 
-
-// ---------------------------------------
-// Get All Interviews
-// ---------------------------------------
-
 export const getAllInterviews = async () => {
-
   try {
     const response = await api.get(`/api/interview/all`);
-
   return response.data;
-
-  } catch (error) {
+  } catch (error: any) {
     console.log(error)
     return null
   }
-
-  
-
 };
