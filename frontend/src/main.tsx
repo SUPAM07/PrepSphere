8db39af999cba9141dd8ca8b01bd04d2f936aaa5
SUPAM_07 <supamroy8@gmail.com>
2026-08-07 14:29:08 +0530

    refactor(frontend): convert main.jsx to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from "react-redux";
-import store from './redux/store.js'
+import store from './redux/store'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
 
     <BrowserRouter>
     <Provider store={store}>
